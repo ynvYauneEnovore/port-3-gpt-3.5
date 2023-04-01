4d4e9d6f6cd035b59db8e2974b643d865d1f9c9a
ynvYauneEnovore <yovanuxf@gmail.com>
2023-04-01 18:14:12 -0400

    update-01-04-2023-18-14-09

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -11,23 +11,90 @@
         <q-btn-dropdown class="glossy" label="DEV">
           <div class="row no-wrap q-pa-md">
             <div class="column">
+              <q-btn dense flat icon="close" close>
+                <q-tooltip class="bg-white text-primary">Close</q-tooltip>
+              </q-btn>
               <div class="text-h6 q-mb-md">Información</div>
               <q-toggle
                 color="positive"
                 v-model="mobileData"
-                label="Disponible"
+                label="Dark"
+                disable
               />
-              <q-toggle v-model="bluetooth" label="Bluetooth" />
+              <q-toggle v-model="bluetooth" label="Bluetooth" disable />
               <br /><q-separator inset /><br />
-
-              <q-btn padding="xs lg" color="positive" round>
+              <q-toggle v-model="bluetooth" label="En desarrollo" disable />
+              <br /><q-separator inset /><br />
+              chatBot
+              <q-btn
+                padding="xs lg"
+                color="positive"
+                round
+                @click="bar2 = true"
+              >
                 <q-spinner-comment color="dark" size="2em" />
                 <q-icon color="dark" size="4em" name="smart_toy"></q-icon>
               </q-btn>
+              <q-dialog
+                v-model="bar2"
+                persistent
+                transition-show="flip-down"
+                transition-hide="flip-up"
+              >
+                <q-card class="bg-primary text-white">
+                  <q-bar>
+                    <q-icon name="network_wifi" />
+                    <q-icon name="network_cell" />
+                    <q-icon name="battery_full" />
+
+                    <q-space />
+
+                    <q-btn dense flat icon="close" close>
+                      <q-tooltip class="bg-white text-primary">Close</q-tooltip>
+                    </q-btn>
+                  </q-bar>
+
+                  <q-card-section>
+                    <div class="text-h6">
+                      Quieres que te avise cuando el sistema chatBot este
+                      completemente integrado en mi web. Por ahora el modelo
+                      esta en desarrollo esta funcionando solo para los dev. si
+                      quieres realizr test avisame por este medio.
+
+                      <form @submit.prevent="simulateSubmit" class="q-pa-md">
+                        <!-- a simple text field watching for the enter key release -->
+                        <q-input
+                          filled
+                          color="teal"
+                          hint="Type then hit Enter key above"
+                          v-model="test"
+                        />
+
+                        <!--
+      A button with v-model set to submit.
+      v-model scope variable must be a strict Boolean
+    -->
+                        <div class="row justify-end">
+                          <q-btn
+                            type="submit"
+                            :loading="submitting"
+                            label="Save"
+                            class="q-mt-md"
+                            color="teal"
+                          >
+                            <template v-slot:loading>
+                              <q-spinner-facebook />
+                            </template>
+                          </q-btn>
+                        </div>
+                      </form>
+                    </div>
+                  </q-card-section>
+                </q-card>
+              </q-dialog>
             </div>
 
             <q-separator vertical inset class="q-mx-lg" />
-
             <div class="column items-center">
               <q-avatar size="72px">
                 <img src="img/perf.png" />
@@ -41,6 +108,9 @@
               <q-btn color="positive" @click="persistent = true">
                 ir \ system<q-spinner-gears color="dark" size="2.5em" />
               </q-btn>
+
+              <q-toggle v-model="activo" label="activo" />
+              <br /><q-separator inset /><br />
 
               <q-dialog
                 v-model="persistent"
@@ -87,7 +157,7 @@
             direction="up"
             style="background-color: rgba(1, 31, 43, 0.81)"
           >
-            <q-fab-action @click="onClick" color="dark">
+            <q-fab-action @click="call" color="dark">
               <q-icon name="build"></q-icon>
             </q-fab-action>
 
@@ -100,11 +170,11 @@
             </q-fab-action>
 
             <q-fab-action @click="onClick" color="dark">
-              <q-icon name="build"></q-icon>
+              <q-icon name="facebook"></q-icon>
             </q-fab-action>
 
             <q-fab-action @click="onClick" color="dark">
-              <q-icon name="facebook"></q-icon>
+              <q-icon name="mail"></q-icon>
             </q-fab-action>
           </q-fab>
         </q-page-sticky>
@@ -125,7 +195,7 @@ import { watch } from "vue";
 export default defineComponent({
   methods: {
     IP_YNV() {
-      window.location.href = "http://192.168.1.1";
+      window.location.href = "https://ynv-servis.tech";
     },
   },
 
@@ -134,6 +204,21 @@ export default defineComponent({
   setup() {
     const $q = useQuasar();
     const leftDrawerOpen = ref(false);
+    const test = ref("");
+    const submitting = ref(false);
+
+    function simulateSubmit() {
+      submitting.value = true;
+      // Simulating a delay here.
+      // When we are done, we reset "submitting"
+      // Boolean to false to restore the
+      // initial state.
+      setTimeout(() => {
+        // delay simulated, we are done,
+        // now restoring submit to its initial state
+        submitting.value = false;
+      }, 3000);
+    }
 
     watch(
       () => $q.dark.isActive,
@@ -143,13 +228,22 @@ export default defineComponent({
     );
 
     return {
+      test,
+      submitting,
+      simulateSubmit,
+      bar2: ref(false),
       seamless: ref(false),
       persistent: ref(false),
       onClick() {
         console.log("Clicked on a fab action");
       },
-      mobileData: ref(true),
-      bluetooth: ref(true),
+      call() {
+        alert("Clicked on a fab action");
+      },
+      activo: ref(true),
+      endesarrollo: ref(false),
+      mobileData: ref(false),
+      bluetooth: ref(false),
       leftDrawerOpen,
       toggleLeftDrawer() {
         leftDrawerOpen.value = !leftDrawerOpen.value;
